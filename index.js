bd8c6e5ac102e9c7321a3a7c944797aa935ca870
Zachary Youngdale <zyoungdale@gmail.com>
2022-08-31 14:16:35 -0400

    commit tree 9abebc99d936df74b52ad33118aba88978020c19

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here
+function findMatching(array, string) {
+    const result = array.filter((el) => el.toLowerCase().includes(string.toLowerCase()));
+    return result;
+  }
+
+  function fuzzyMatch (array, string) {
+    const result = array.filter(element => element.startsWith(string))
+  return result
+}
+
+function matchName (array, string) {
+  return array.filter(element => element.name === string);
+}
